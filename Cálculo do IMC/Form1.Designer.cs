96d2f7399054d92b9ef1cb1a099a6d545d6ef36f
andressagomes2 <117682646+andressagomes2@users.noreply.github.com>
2022-11-08 16:25:43 -0300

    Initial commit

---
+++ Cálculo do IMC/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Cálculo_do_IMC
+{
+    partial class IMC
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IMC));
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txb1 = new System.Windows.Forms.TextBox();
+            this.txb2 = new System.Windows.Forms.TextBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txb3 = new System.Windows.Forms.TextBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.txbresposta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(34, 19);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(174, 25);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Calcule seu IMC";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(26, 59);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(54, 24);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Peso ";
+            // 
+            // txb1
+            // 
+            this.txb1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb1.Location = new System.Drawing.Point(94, 55);
+            this.txb1.Name = "txb1";
+            this.txb1.Size = new System.Drawing.Size(100, 32);
+            this.txb1.TabIndex = 2;
+            // 
+            // txb2
+            // 
+            this.txb2.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb2.Location = new System.Drawing.Point(94, 114);
+            this.txb2.Name = "txb2";
+            this.txb2.Size = new System.Drawing.Size(100, 32);
+            this.txb2.TabIndex = 4;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(14, 114);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(66, 24);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "Altura";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(60, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 52);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IMC";
+            // 
+            // txb3
+            // 
+            this.txb3.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb3.Location = new System.Drawing.Point(94, 239);
+            this.txb3.Name = "txb3";
+            this.txb3.Size = new System.Drawing.Size(100, 32);
+            this.txb3.TabIndex = 7;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(45, 293);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(0, 24);
+            this.lbl4.TabIndex = 8;
+            // 
+            // txbresposta
+            // 
+            this.txbresposta.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbresposta.Location = new System.Drawing.Point(18, 297);
+            this.txbresposta.Name = "txbresposta";
+            this.txbresposta.Size = new System.Drawing.Size(205, 24);
+            this.txbresposta.TabIndex = 9;
+            // 
+            // IMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 331);
+            this.Controls.Add(this.txbresposta);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.txb3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txb2);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.txb1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "IMC";
+            this.Text = "Cálculo do IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txb1;
+        private System.Windows.Forms.TextBox txb2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txb3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.TextBox txbresposta;
+    }
+}
+
---
+++ Cálculo do IMC/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cálculo_do_IMC
+{
+    public partial class IMC : Form
+    {
+        public IMC()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double p = double.Parse(txb1.Text);
+            double a = double.Parse(txb2.Text);
+            double r = p / (a*a) ;
+            txb3.Text = p / (a * a) + "";
+            if (r <= 18.5 )
+            {
+               txbresposta.Text=("Seu peso está abaixo do Ideal");
+            }
+            else if (r >= 18.6)
+            {
+                txbresposta.Text = ("Show Dez Jovem!!! Peso Ideal");
+            }
+            else if (r <= 25)
+            {
+                txbresposta.Text = ("Levemente acima, ok Jovem?!");
+            }
+            else if (r <= 30)
+            {
+                txbresposta.Text = ("Obesidade grau I, precisa melhorar Jovem.");
+            }
+            else if (r <= 35)
+            {
+                txbresposta.Text = ("Obesidade grau II, Complicado Jovem!");
+            }
+            else if (r <= 40)
+            {
+                txbresposta.Text = ("Obesidade grau III, Putz Jovem!");
+            }
+        }
+
+  
+    }
+}
